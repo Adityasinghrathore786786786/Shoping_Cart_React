37ca48b83d8c60067ee3c8291e4c62adc16c31a1
Aditya <adityasinghsmpr786@gmail.com>
2023-05-03 17:10:11 +0530

    product and home component ready

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,52 @@
+import { useEffect, useState } from "react";
+import Spinner from "../components/Spinner";
+import Product from "../components/Product";
+
 const Home = () => {
   const API_URL = "https://fakestoreapi.com/products";
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([])
+
+  async function fetchProductData (){
+    setLoading(true);
+    try {
+      const res = await fetch(API_URL);
+      const data = await res.json();
+      setPosts(data);
+      
+    } catch (error) {
+      console.log("Error aaa gaya");
+      setPosts([]);
+    }
+    setLoading(false);
+  }
+
+  useEffect(()=>{
+    fetchProductData();
+  }, [])
 
   return (
-    <div>This is Home page</div>
+    <div>
+      {
+        loading ? <Spinner/> : 
+          posts.length > 0 ? (
+            <div>
+              {
+                posts.map((post)=>(
+                  <Product key={post.id} post={post}/>
+                )
+                  
+                )
+              }
+              
+            </div>
+            
+          ):
+            <div>"No Data Found"</div>
+          
+        
+      }
+    </div>
   );
 };
 
