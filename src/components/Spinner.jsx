046da01a81d912119ca38bc774fe1b6ba73dd7c7
Aditya <adityasinghsmpr786@gmail.com>
2023-05-04 17:22:29 +0530

    Css Updated Project complete

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Spinner = () => {
   return (
-    <div>
-      <div className='spinner'></div>
+    <div className='w-screen h-[calc(100vh-80px)] flex justify-center items-center'>
+      <div className='spinner '></div>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,11 +26,11 @@ const Home = () => {
   }, [])
 
   return (
-    <div>
+    <div >
       {
         loading ? <Spinner/> : 
           posts.length > 0 ? (
-            <div>
+            <div className="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl p-2 mx-auto space-y-10 space-x-5 min-h-[80vh]">
               {
                 posts.map((post)=>(
                   <Product key={post.id} post={post}/>
@@ -42,7 +42,9 @@ const Home = () => {
             </div>
             
           ):
-            <div>"No Data Found"</div>
+            <div className="flex justify-center items-center">
+              "No Data Found"
+            </div>
           
         
       }
